1fa0f3f1012d1827c2cde28fe3d559b01918a5cd
Diddayngo <lamngo606@gmail.com>
2023-10-30 08:34:19 +0200

    pageHeader completed

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,9 +1,94 @@
-import React from "react";
+import { Menu, Upload, Bell, User, Mic, Search, ArrowLeft } from "lucide-react";
+import logo from "../assets/Logo.png";
+import Button from "../components/Button";
+import { useEffect, useState } from "react";
+import useMediaQuery from "../hooks/useMediaQuery";
 
-type Props = {};
+const PageHeader = () => {
+  const [showSearchBar, setShowSearchBar] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 768px)");
 
-const PageHeader = (props: Props) => {
-  return <div>PageHeader</div>;
+  useEffect(() => {
+    isAboveMediumScreens && setShowSearchBar(false);
+    console.log("checked");
+  }, [isAboveMediumScreens]);
+
+  return (
+    <div className="flex items-center justify-between gap-10 lg:gap-20 pt-2 mb-6 mx-4">
+      <div
+        className={`items-center gap-4 flex-shrink-0 ${
+          showSearchBar ? "hidden" : "flex"
+        }`}
+      >
+        <Button variant="ghost" size="icon">
+          <Menu />
+        </Button>
+        <a href="#home">
+          <img src={logo} alt="youtube-symbol" className="h-6" />
+        </a>
+      </div>
+
+      <form
+        className={`gap-4 flex-grow justify-center ${
+          showSearchBar ? "flex" : "hidden md:flex"
+        }`}
+      >
+        <Button
+          type="button"
+          size="icon"
+          variant="ghost"
+          className={`flex-shrink-0 ${!showSearchBar && "hidden"}`}
+          onClick={() => setShowSearchBar(false)}
+        >
+          <ArrowLeft />
+        </Button>
+
+        <div className="flex flex-grow max-w-[600px] ">
+          <input
+            type="search"
+            placeholder="Search"
+            className="w-full outline-none rounded-l-full 
+            border border-secondary-border shadow-inner shadow-secondary
+            py-1 px-4 text-lg focus:border-blue-500"
+          />
+          <Button className="py-2 px-4 rounded-r-full border border-secondary-border border-l-0">
+            <Search />
+          </Button>
+        </div>
+
+        <Button size="icon" className="flex-shrink-0">
+          <Mic />
+        </Button>
+      </form>
+
+      <div
+        className={`md:gap-2 whitespace-nowrap flex-shrink-0 ${
+          showSearchBar ? "hidden" : "flex"
+        }`}
+      >
+        <Button
+          variant="ghost"
+          size="icon"
+          className="md:hidden"
+          onClick={() => setShowSearchBar(true)}
+        >
+          <Search />
+        </Button>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Mic />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Upload />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Bell />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <User />
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default PageHeader;
--- src/sidebar/index.tsx
+++ src/sidebar/index.tsx
@@ -2,7 +2,7 @@ type Props = {};
 
 const Sidebar = (props: Props) => {
   return (
-    <nav className="fixed h-full left-0 top-0 flex flex-col items-center gap-8 z-10 border pt-20 px-4">
+    <nav className="">
       <p>Home</p>
       <p>Shorts</p>
       <p>Subscriptions</p>
